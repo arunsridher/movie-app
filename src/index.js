c52ed3c33798d78a6fb31f0264f0e021882e3a90
arunsridher <45627741+arunsridher@users.noreply.github.com>
2020-04-09 11:22:07 +0530

    adding middlwares and 2 ways in which we can create middleware functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+// //function logger(obj, next, action) -> obj has dispatch and getState
+// //logger is called as a curry function -> logger(obj)(next)(action)
+// const logger = function({dispatch, getState}){
+//   return function(next){
+//     return function(action){
+//       //middleware code
+//       console.log('ACTION_TYPE : ', action.type);
+//       next(action);
+//     }
+//   }
+// }
+
+const logger = ({dispatch, getState}) => (next) => (action) => {
+  //middleware code
+  console.log('ACTION_TYPE : ', action.type);
+  next(action);
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(<App store={store}/>, document.getElementById('root'));
